0ffa69aa50d598238785ca265ea8e080b7fb3f4e
elzel <elzel666@gmail.com>
2024-01-07 20:07:08 +0300

    created_MainController

--- iOS-HW21-Elena_Zelenova/Sources/MainScreen/MainController.swift
+++ iOS-HW21-Elena_Zelenova/Sources/MainScreen/MainController.swift
@@ -3,6 +3,52 @@ import Alamofire
 
 class MainController: UIViewController {
     
- 
+    private var model: [Character] = []
+    
+    private var mainView: MainView? {
+        guard isViewLoaded else { return nil }
+        return view as? MainView
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    // MARK: - Setups
+    
+    private func setupView() {
+        mainView?.backgroundColor = .systemBackground
+        mainView?.tableView.delegate = self
+        mainView?.tableView.dataSource = self
+    }
+}
+
+// MARK: - Extensions
+
+extension MainController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MarvelCell
+        cell?.model = model[indexPath.row]
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension MainController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewController = DetailController()
+        viewController.model = model[indexPath.row]
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 }
 
